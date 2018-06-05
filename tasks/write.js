debf6616fde756fb84a69cfdc6a79ae661f1f349
xiehaoran <546898213@qq.com>
2018-06-05 14:13:24 +0800

    Delete write.js

--- tasks/write.js
+++
@@ -1,9 +0,0 @@
-var Movie = require('../model').Movie;
-var async = require('async');
-var debug = require('debug')('crawl:write');
-exports.movie=function (movies,callback) {
-    async.forEach(movies,function (item,cb) {
-        debug(`保存电影:${item.name}`);
-        Movie.create(item,cb)
-    },callback)
-};
