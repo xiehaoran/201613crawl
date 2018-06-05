08d1e3426983156a61ec06ed6e9e06a8cbbe7a56
xiehaoran <546898213@qq.com>
2018-06-05 14:13:40 +0800

    Delete model.js

--- model.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/201613crawl');
-var MovieSchema = new mongoose.Schema({
-    name:String,
-    url:String
-});
-exports.Movie = mongoose.model('Movie',MovieSchema);
